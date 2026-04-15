c399a1f1327fecc27468937407170daaffcd5b02
Razigue Ben Hmida <razigue.benhmida@epitech.eu>
2026-04-15 06:05:36 +0200

    add: yml code to push on pages

--- tests/twoships.test.js
+++ tests/twoships.test.js
@@ -1,5 +1,29 @@
-import { randFloatSpread, mapLinear, lerp } from '../src/math';
+import {
+  randFloatSpread,
+  mapLinear,
+  lerp,
+  clamp,
+  randFloat,
+} from '../src/math';
 
 test('randFloatSpread(1) <= 1 return true', () => {
   expect(randFloatSpread(1) <= 1).toBe(true);
+});
+
+test('clamp(6,1,5) must return 5', () => {
+  expect(clamp(6, 1, 5)).toBe(5);
+});
+
+test('mapLinear(1,2,3,4,5) must return 3', () => {
+  expect(mapLinear(1, 2, 3, 4, 5)).toBe(3);
+});
+
+test('lerp(1,3,20) must return 41', () => {
+  expect(lerp(1, 3, 20)).toBe(41);
+});
+
+test('randFloat(5, 10) returns a value between 5 and 10', () => {
+  const result = randFloat(5, 10);
+  expect(result).toBeGreaterThanOrEqual(5);
+  expect(result).toBeLessThanOrEqual(10);
 });
