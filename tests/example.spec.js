aa1d86cd0771cf8199b71c92577aaada219c81bd
Razigue Ben Hmida <razigue.benhmida@epitech.eu>
2026-04-17 15:02:18 +0200

    fix

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,13 +2,13 @@
 import { test, expect } from '@playwright/test';
 
 test('quand la page se charge le canva est visible', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000/');
   const canvas = page.locator('canvas');
   await expect(canvas).toBeVisible();
 });
 
 test('quand on clique sur le jeu les hp sont à 100', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000/');
   const health = await page.textContent('.h');
   await page.locator('body').click();
   expect(health).toBe('100');
@@ -18,11 +18,11 @@ test('quand on lance le jeu et attend 15 seconde on meurt', async ({
   page,
 }) => {
   test.setTimeout(120_000);
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000/');
   await page.waitForSelector('canvas');
   await page.locator('body').click();
   await page.waitForTimeout(25000);
-  expect(page.locator('.n')).toBeVisible({ timeout: 25000 });
+  expect(page.locator('.n')).toBeVisible();
 });
 //   // Expect a title "to contain" a substring.
 //   await expect(page).toHaveTitle(/Playwright/);
