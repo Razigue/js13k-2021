4725db40123c09db2ba09ca7ed0e575f003c3497
Razigue Ben Hmida <razigue.benhmida@epitech.eu>
2026-04-19 20:45:08 +0200

    add: more unit test

--- tests/twoships.test.js
+++ tests/twoships.test.js
@@ -10,12 +10,24 @@ test('randFloatSpread(1) <= 1 return true', () => {
   expect(randFloatSpread(1) <= 1).toBe(true);
 });
 
+test('randFloatSpread(1) >= -1 returns true', () => {
+  expect(randFloatSpread(1) >= -1).toBe(true);
+});
+
 test('mapLinear(1,2,3,4,5) must return 3', () => {
   expect(mapLinear(1, 2, 3, 4, 5)).toBe(3);
 });
 
+test('mapLinear(1,20,3,40,5) returns 0.882352941176471', () => {
+  expect(mapLinear(1, 20, 3, 40, 5)).toBeCloseTo(0.882352941176471);
+});
+
 test('lerp(1,3,20) must return 41', () => {
   expect(lerp(1, 3, 20)).toBe(41);
+});
+
+test('lerp(1.3,-7,2) returns -15.3', () => {
+  expect(lerp(1.3, -7, 2)).toBeCloseTo(-15.3);
 });
 
 test('randFloat(5, 10) returns a value between 5 and 10', () => {
